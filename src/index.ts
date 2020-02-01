a0721416f01fe19e0c46f7ac25a3461a9d95db22
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2020-02-01 17:20:19 +0100

    13 - Nuestro primer Observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
+import { Observable } from 'rxjs';
 
+// Para crear Observables hay diversas formas:
 
-console.log('Ernesto Antonio!');
+// 1) Usando el metodo estatico create(). Es poco comun.
+// const obs$ = Observable.create();
 
+const obs$ = new Observable<string>(subs => {
+    subs.next('Hola');
+    subs.next('Mundo');
 
+    subs.next('Hola');
+    subs.next('Mundo');
 
+    subs.complete();
 
+    subs.next('Hola');
+    subs.next('Mundo');
+});
 
-
-
+// Imprimir los datos recibidos en consola
+obs$.subscribe( console.log);

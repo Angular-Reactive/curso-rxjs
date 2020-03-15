df098afa1802adce22b18c24b4801c885c4eabff
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2020-03-15 18:15:51 +0100

    53- Operador distinctUntilChanged

--- src/operadores-basicos/12.distinctUntilChanged.ts
+++ src/operadores-basicos/12.distinctUntilChanged.ts
@@ -1,5 +1,16 @@
 import { of, from } from 'rxjs';
 import { distinct, distinctUntilChanged, pluck } from 'rxjs/operators';
+/**
+ * El operador "distinct" deja pasar
+ * ùnicamente los valores que no han sido
+ * previamente emitidos por el Observable.
+ */
+
+ const numeros$ = of<number | string>(1, '1', 1, 1, 3, 3, 2, 2, 4, 4, 5, 3, 1, '1');
+
+ numeros$.pipe(
+     distinctUntilChanged()
+ ).subscribe(val => console.log('valor: ', val));
 
  interface Personaje  {
      nombre: string;

bf86df0f24622665a2a712ea80c2bf66ea1c446f
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2020-03-17 11:45:19 +0100

    76 - Operador mergeAll

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { ajax } from 'rxjs/ajax';
 import { fromEvent } from 'rxjs';
-import { debounceTime, map, pluck } from 'rxjs/operators';
+import { debounceTime, map, pluck, mergeAll } from 'rxjs/operators';
 
 // Referencias
 const body = document.querySelector('body');
@@ -8,19 +8,24 @@ const textInput = document.createElement('input');
 const orderList = document.createElement('ol');
 body.append(textInput, orderList);
 
+// El operador 'mergeAll' sirve para trabajar con Observables
+// que internamente retornan Observables.
+// Hasta que todos los Observables hijos del 'mergeAll' no se completen
+// no se a a disparar el complete de la subscripciòn inicial.
+// Este procedimiento de unificar Observables en una sola salida, se conoce
+// como 'Flattening Operator' o 'Operador de aplanamiento'.
+
 // Streams
 const input$ = fromEvent<KeyboardEvent>(textInput, 'keyup');
 
 input$.pipe(
     debounceTime(500),
-    map(event => {
-        const texto = event.target['value'];
-        return ajax.getJSON(
-            `https://api.github.com/users/${texto}`)
-    })
+    pluck('target', 'value'),
+    map(texto => ajax.getJSON(
+            `https://api.github.com/search/users?q=${texto}`)
+    ),
+    mergeAll(),
+    pluck('items')
 ).subscribe(resp => {
-    resp.pipe(
-        pluck('url')
-    )
-    .subscribe(console.log)
+    console.log(resp)
 });
